51a78a2fcac00b7e512d7abdacffd5a1c3a12388
kyem-asa <turtlemoons@protonmail.com>
2022-06-30 17:52:32 -0500

    Add button state

---
+++ main.js
@@ -0,0 +1,6 @@
+document.querySelector('.rating__btn').addEventListener('click', function(){
+  document.querySelector('.rating').style.display = 'none'
+  document.querySelector('.submitted-state').style.display = 'flex'
+})
+
+
