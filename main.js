a9e321ee703c59119ee2b714df7ba22e1bfd17ca
kyem-asa <turtlemoons@protonmail.com>
2022-07-02 23:56:11 -0500

    Reset functions

--- main.js
+++
@@ -1,6 +0,0 @@
-document.querySelector('.rating__btn').addEventListener('click', function(){
-  document.querySelector('.rating').style.display = 'none'
-  document.querySelector('.submitted-state').style.display = 'flex'
-})
-
-
