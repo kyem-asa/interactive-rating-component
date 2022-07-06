417f8db12d3659ea908e29647ea81e6326d861c0
kyem-asa <turtlemoons@protonmail.com>
2022-07-06 18:43:53 -0500

    Refactor js

--- main.js
+++ main.js
@@ -1,60 +1,29 @@
+let clickedRating = ''
 
-document.getElementById('rating-one').addEventListener('click', function(){
-  let clickedRating = '1'
-  console.log(clickedRating)
-
-  document.querySelector('.rating__btn').addEventListener('click', function(){
-    
-    document.querySelector('.rating').style.display = 'none'
-    document.querySelector('.submitted-state').style.display = 'flex'
-    document.querySelector('.submitted-state__selection').innerHTML = `You selected ${clickedRating} out of 5`
+document.querySelector('.rating__submissions').addEventListener('mouseover', function(){
+  document.getElementById('rating-one').addEventListener('click', function(){
+  clickedRating = '1'
+})
+  document.getElementById('rating-two').addEventListener('click', function(){
+  clickedRating = '2'
+})
+  document.getElementById('rating-three').addEventListener('click', function(){
+  clickedRating = '3'
+})
+  document.getElementById('rating-four').addEventListener('click', function(){
+  clickedRating = '4'
+})
+  document.getElementById('rating-five').addEventListener('click', function(){
+  clickedRating = '5'
   })
 })
 
-document.getElementById('rating-two').addEventListener('click', function(){
-  let clickedRating = '2'
-  console.log(clickedRating)
+document.querySelector('.rating__btn').addEventListener('click', function(){
+  document.querySelector('.rating').style.display = 'none'
+  document.querySelector('.submitted-state').style.display = 'flex'
 
-  document.querySelector('.rating__btn').addEventListener('click', function(){
-    
-    document.querySelector('.rating').style.display = 'none'
-    document.querySelector('.submitted-state').style.display = 'flex'
-    document.querySelector('.submitted-state__selection').innerHTML = `You selected ${clickedRating} out of 5`
+  document.querySelector('.submitted-state__selection').textContent = `You rated ${clickedRating} out of 5`
+  // console.log(`You clicked ${clickedRating}`)
   })
-})
 
-document.getElementById('rating-three').addEventListener('click', function(){
-  let clickedRating = '3'
-  console.log(clickedRating)
 
-  document.querySelector('.rating__btn').addEventListener('click', function(){
-    
-    document.querySelector('.rating').style.display = 'none'
-    document.querySelector('.submitted-state').style.display = 'flex'
-    document.querySelector('.submitted-state__selection').innerHTML = `You selected ${clickedRating} out of 5`
-  })
-})
-
-document.getElementById('rating-four').addEventListener('click', function(){
-  let clickedRating = '4'
-  console.log(clickedRating)
-
-  document.querySelector('.rating__btn').addEventListener('click', function(){
-    
-    document.querySelector('.rating').style.display = 'none'
-    document.querySelector('.submitted-state').style.display = 'flex'
-    document.querySelector('.submitted-state__selection').innerHTML = `You selected ${clickedRating} out of 5`
-  })
-})
-
-document.getElementById('rating-five').addEventListener('click', function(){
-  let clickedRating = '5'
-  console.log(clickedRating)
-
-  document.querySelector('.rating__btn').addEventListener('click', function(){
-    
-    document.querySelector('.rating').style.display = 'none'
-    document.querySelector('.submitted-state').style.display = 'flex'
-    document.querySelector('.submitted-state__selection').innerHTML = `You selected ${clickedRating} out of 5`
-  })
-})
