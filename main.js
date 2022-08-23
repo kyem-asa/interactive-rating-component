e4b29db97dfc82eaecc79c3c097e7a2c39ece89f
kyem-asa <turtlemoons@protonmail.com>
2022-08-23 09:13:16 -0500

    Clean up formatting

--- main.js
+++ main.js
@@ -1,29 +1,40 @@
-let clickedRating = ''
+let clickedRating = "";
 
-document.querySelector('.rating__submissions').addEventListener('mouseover', function(){
-  document.getElementById('rating-one').addEventListener('click', function(){
-  clickedRating = '1'
-})
-  document.getElementById('rating-two').addEventListener('click', function(){
-  clickedRating = '2'
-})
-  document.getElementById('rating-three').addEventListener('click', function(){
-  clickedRating = '3'
-})
-  document.getElementById('rating-four').addEventListener('click', function(){
-  clickedRating = '4'
-})
-  document.getElementById('rating-five').addEventListener('click', function(){
-  clickedRating = '5'
-  })
-})
+document
+  .querySelector(".rating__submissions")
+  .addEventListener("mouseover", function () {
+    document
+      .getElementById("rating-one")
+      .addEventListener("click", function () {
+        clickedRating = "1";
+      });
+    document
+      .getElementById("rating-two")
+      .addEventListener("click", function () {
+        clickedRating = "2";
+      });
+    document
+      .getElementById("rating-three")
+      .addEventListener("click", function () {
+        clickedRating = "3";
+      });
+    document
+      .getElementById("rating-four")
+      .addEventListener("click", function () {
+        clickedRating = "4";
+      });
+    document
+      .getElementById("rating-five")
+      .addEventListener("click", function () {
+        clickedRating = "5";
+      });
+  });
 
-document.querySelector('.rating__btn').addEventListener('click', function(){
-  document.querySelector('.rating').style.display = 'none'
-  document.querySelector('.submitted-state').style.display = 'flex'
+document.querySelector(".rating__btn").addEventListener("click", function () {
+  document.querySelector(".rating").style.display = "none";
+  document.querySelector(".submitted-state").style.display = "flex";
 
-  document.querySelector('.submitted-state__selection').textContent = `You rated ${clickedRating} out of 5`
-  // console.log(`You clicked ${clickedRating}`)
-  })
-
-
+  document.querySelector(
+    ".submitted-state__selection"
+  ).textContent = `You rated ${clickedRating} out of 5`;
+});
